1bf67c57e1c853726c7479fb5493a5e625733ae4
FateAssassin <muazugur@gmail.com>
2024-06-05 23:21:07 +0200

    added footer and updated app.js

--- src/App.js
+++ src/App.js
@@ -1,75 +1,14 @@
 import './App.css';
-import { useState } from 'react';
+import ToDo from './components/ToDo';
+import Footer from './components/Footer';
 
 function App() {
 
-  const [todo, setTodo] = useState('');
-
-  const [todos, setTodos] = useState(JSON.parse(localStorage.getItem('todos')) || []);
-
-
-
-  function addTodo(){
-    if (todo === '') {
-      return;
-    }
-    if (localStorage.getItem('todos') === null) {
-      localStorage.setItem('todos', JSON.stringify([]));
-    }
-    var todoItems = JSON.parse(localStorage.getItem("todos"));
-    var id = null;
-    try{id = todoItems[todoItems.length - 1].id + 1;}
-    catch{id = 1;}
-
-    todoItems.push({
-        "id": id,
-        "todo": todo
-      }
-    );
-    setTodos(todoItems);
-    localStorage.setItem('todos', JSON.stringify(todoItems));
-    setTodo('');
-  }
-  function deleteTodo(item){
-    var todoItems = JSON.parse(localStorage.getItem("todos"));
-    var newItems = todoItems.filter((i) => i.id !== item.id);
-    setTodos(newItems);
-    localStorage.setItem('todos', JSON.stringify(newItems));
-  }
-  function clearTodo(){
-    localStorage.setItem('todos', JSON.stringify([]));
-    setTodos([]);
-  }
-
   return (
-    <div className='h-screen flex justify-center items-center flex-shrink-0'>
-      <div className='w-2/5'>
-        <div className='border rounded-xl'>
-          <div className=' bg-purple-300 p-2 flex justify-center rounded-t-xl'>
-            <h1 className='text-3xl font-semibold'>ToDo-List</h1>
-          </div>
-          
-          <div className='h-96 overflow-y-scroll'>
-
-          {todos.map((item) =>{
-            return (
-              <div key={item.id} className='flex justify-between bg-purple-100 p-2 m-2 rounded-xl'>
-                <p className='text-3xl ml-3'>{item.todo}</p>
-                <button className='bg-red-300 mr-3 rounded-xl text-3xl py-1 px-3 font-light active:bg-red-400 active:text-red-900 duration-75' onClick={(e) => deleteTodo(item)}>Delete</button>
-              </div>
-            );
-          })}
-
-          </div>
-        
-          <div className=' bg-purple-300 p-2 flex justify-center rounded-b-xl mt-2'>
-            <input type='text' className='ml-5 mr-3 p-1 rounded-md focus:bg-gray-100 focus:outline-none' value={todo} onKeyDown={(e) => e.key === 'Enter' && addTodo()} required onChange={(e) => setTodo(e.target.value)} placeholder='Add ToDo'/>
-            <button className='mr-10 ml-5 bg-blue-300 rounded-xl py-1 px-3 font-semibold active:bg-blue-400 active:text-blue-900 duration-75' onClick={addTodo}>Add</button>
-            <button className='bg-red-300 rounded-xl py-1 px-3 font-semibold active:bg-red-400 active:text-red-900 duration-75' onClick={clearTodo}>Clear</button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <>
+      <ToDo/>
+      <Footer/>
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return (
+        <div className="fixed w-screen bottom-0 text-center bg-slate-500 p-3">
+            <p className="text-xl text-white">Made by <a className=" text-blue-300 hover:text-blue-700 duration-300" href="https://github.com/FateAssassin">FateAssassin</a> | <a className="hover:text-gray-300 duration-300" href="https://github.com/FateAssassin/ToDo-App">Source Code</a></p>
+        </div>
+    )
+}
